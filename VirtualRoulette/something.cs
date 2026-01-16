d156aaf47493b7dd093d0edd73104b3a572a4f6a
lkarzhalovi <karzhalovi@gmail.com>
2026-01-17 00:25:16 +0400

    Add reference to ge.singular.roulette.dll in project file

---
+++ VirtualRoulette/something.cs
@@ -0,0 +1,13 @@
+namespace VirtualRoulette;
+using ge.singular.roulette;
+
+
+public class something
+{
+    void something1()
+    {
+        var bet = "123";
+        IsBetValidResponse ibvr = CheckBets.IsValid(bet);
+        
+    }
+}
